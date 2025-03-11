4194c5c1af342de8e2d50d5fb837a80b97824a45
jakubdziworski <jakub.dziworski@gmail.com>
2025-03-11 20:14:19 +0100

    Iniital commit

---
+++ src/main.cpp
@@ -0,0 +1,231 @@
+/*
+SerialPassthrough sketch
+
+  Some boards, like the Arduino 101, the MKR1000, Zero, or the Micro, have one
+  hardware serial port attached to Digital pins 0-1, and a separate USB serial
+  port attached to the IDE Serial Monitor. This means that the "serial
+  passthrough" which is possible with the Arduino UNO (commonly used to interact
+  with devices/shields that require configuration via serial AT commands) will
+  not work by default.
+
+  This sketch allows you to emulate the serial passthrough behaviour. Any text
+  you type in the IDE Serial monitor will be written out to the serial port on
+  Digital pins 0 and 1, and vice-versa.
+
+  On the 101, MKR1000, Zero, and Micro, "Serial" refers to the USB Serial port
+  attached to the Serial Monitor, and "Serial1" refers to the hardware serial
+  port attached to pins 0 and 1. This sketch will emulate Serial passthrough
+  using those two Serial ports on the boards mentioned above, but you can change
+  these names to connect any two serial ports on a board that has multiple ports.
+
+  created 23 May 2016
+  by Erik Nyquist
+*/
+
+#include "ELMduino.h"
+#include "Arduino.h"
+#include "ui.hpp"
+
+
+#define SerialELM Serial2
+ELM327 elmduino;
+
+// Pierwszy CPU - OBD (non-blocking) i display
+struct OBDTask {
+    const char *name;
+
+    void (*function)();
+
+    unsigned long interval;
+    unsigned long lastRun;
+};
+
+OBDTask *currentTask = nullptr;
+unsigned long lastFuelTrimChartUpdate = 0;
+float lastStft1 = 0;
+float lastStft2 = 0;
+float lastLtft1 = 0;
+float lastLtft2 = 0;
+
+void kphTask() {
+    auto kph = elmduino.kph();
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        Serial.println("kph" + kph);
+        ui_setSpeedValue(kph);
+    }
+}
+
+void rpmTask() {
+    auto rpm = elmduino.rpm();
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        Serial.print("rpm: ");
+        Serial.println(rpm);
+        ui_updateFuelTrimChart(rpm / 100, -10);
+    }
+}
+
+void stft1Task() {
+    lastStft1 = elmduino.shortTermFuelTrimBank_1();
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        ui_updateStft1Label(lastStft1);
+    }
+}
+
+void stft2Task() {
+    lastStft2 = elmduino.shortTermFuelTrimBank_2();
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        ui_updateStft2Label(lastStft2);
+    }
+}
+
+void ltft1Task() {
+    lastLtft1 = elmduino.longTermFuelTrimBank_1();
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        ui_updateLtft1Label(lastLtft1);
+    }
+}
+
+void ltft2Task() {
+    lastLtft2 = elmduino.longTermFuelTrimBank_2();
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        ui_updateLtft2Label(lastLtft2);
+    }
+}
+
+void dtcTask() {
+    elmduino.currentDTCCodes(false);
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        Serial.println("found codes: ");
+        ui_updateWarningLabel("");
+        for (const auto code: elmduino.DTC_Response.codes) {
+            ui_updateWarningLabel(code, true);
+            Serial.println(code);
+        }
+        ui_updateWarningLabel("DTCS: ", true);
+    }
+}
+
+static OBDTask tasks[7] = {
+    OBDTask{"kph", kphTask, 50, 0},
+    OBDTask{"rpm", rpmTask, 50, 0},
+    OBDTask{"stft1", stft1Task, 50, 0},
+    OBDTask{"stft2", stft2Task, 50, 0},
+    OBDTask{"ltft2", ltft1Task, 50, 0},
+    OBDTask{"ltft2", ltft2Task, 50, 0},
+    OBDTask{"dtc", dtcTask, 5000, 0},
+};
+
+void finalizeTaskIfDone() {
+    if (elmduino.nb_rx_state == ELM_SUCCESS) {
+        currentTask->lastRun = millis();
+        currentTask = nullptr;
+    } else if (elmduino.nb_rx_state != ELM_GETTING_MSG) {
+        const auto nb_rx_state = elmduino.nb_rx_state;
+        String error_message = "";
+        if (nb_rx_state == ELM_SUCCESS)
+            error_message = "ELM_SUCCESS";
+        else if (nb_rx_state == ELM_NO_RESPONSE)
+            error_message = "ERROR: ELM_NO_RESPONSE";
+        else if (nb_rx_state == ELM_BUFFER_OVERFLOW)
+            error_message = "ERROR: ELM_BUFFER_OVERFLOW";
+        else if (nb_rx_state == ELM_UNABLE_TO_CONNECT)
+            error_message = "ERROR: ELM_UNABLE_TO_CONNECT";
+        else if (nb_rx_state == ELM_NO_DATA)
+            error_message = "ERROR: ELM_NO_DATA";
+        else if (nb_rx_state == ELM_STOPPED)
+            error_message = "ERROR: ELM_STOPPED";
+        else if (nb_rx_state == ELM_TIMEOUT)
+            error_message = "ERROR: ELM_TIMEOUT";
+        else if (nb_rx_state == ELM_BUFFER_OVERFLOW)
+            error_message = "ERROR: BUFFER OVERFLOW";
+        else if (nb_rx_state == ELM_GENERAL_ERROR)
+            error_message = "ERROR: ELM_GENERAL_ERROR";
+        else
+            error_message = "No error detected";
+
+        error_message = String("Task '") + currentTask->name + "' failed  - " + error_message;
+        ui_updateWarningLabel(error_message.c_str());
+        ui_loop();
+        elmduino.printError();
+        currentTask->lastRun = millis();
+        currentTask = nullptr;
+        delay(2000);
+    }
+}
+
+void maybeSubmitFuelTrimChartChanges() {
+    boolean submitFuelTrimChartUpdate = true;
+    for (const auto &task: tasks) {
+        const auto isFuelTrimTask = task.name == "stft1" || task.name == "stft2" || task.name == "ltft1" || task.name == "ltft2";
+        if (isFuelTrimTask &&  task.lastRun < lastFuelTrimChartUpdate) {
+            submitFuelTrimChartUpdate = false; // We haven't received updates from all tasks yet
+            break;
+        }
+    }
+    if (submitFuelTrimChartUpdate) {
+        ui_updateFuelTrimChart(lastStft1 + lastLtft1, lastStft2 + lastLtft2);
+        lastFuelTrimChartUpdate = millis();
+    }
+}
+
+
+// OBD generic stuff //
+void connectOBD() {
+    Serial.println("Connecting");
+    if (!elmduino.begin(SerialELM, true)) {
+        Serial.println("Couldn't connect to OBD scanner");
+        return;
+    }
+    Serial.println("Connected to OBD scanner");
+}
+
+// End OBD generic stuff //
+
+void setup() {
+    delay(500);
+    Serial.begin(115200);
+    SerialELM.begin(38400, SERIAL_8N1, 45, 39);
+    ui_setup();
+}
+
+void executeOrPickNextTask() {
+    unsigned long currentMillis = millis();
+    if (currentTask != nullptr) {
+        currentTask->function();
+        finalizeTaskIfDone();
+    } else {
+        // pick next task
+        unsigned long maxOverdue = 0;
+        for (auto &task: tasks) {
+            unsigned long expectedTime = task.lastRun + task.interval;
+            if (currentMillis >= expectedTime) {
+                unsigned long overdue = currentMillis - expectedTime;
+                if (overdue > maxOverdue) {
+                    maxOverdue = overdue;
+                    currentTask = &task;
+                }
+            }
+        }
+        Serial.println(String("Starting task ") + currentTask->name);
+        ui_updateWarningLabel((String("TASK: ") + currentTask->name).c_str());
+    }
+
+
+}
+
+void loop() {
+    ui_loop();
+    if (!elmduino.connected) {
+        ui_updateWarningLabel("Connecting...");
+        ui_loop();
+        connectOBD();
+        ui_updateWarningLabel("");
+        delay(200);
+        return;
+    }
+
+    executeOrPickNextTask();
+    maybeSubmitFuelTrimChartChanges();
+
+    delay(10);
+}
---
+++ src/ui.hpp
@@ -0,0 +1,228 @@
+#include <LilyGo_AMOLED.h>
+#include <LV_Helper.h>
+#include "Arduino.h"
+
+static lv_obj_t *ui_speedArc;
+static lv_obj_t *ui_speedLabel;
+static lv_obj_t *ui_warningsLabel;
+static lv_obj_t *ui_fuelTrimChart;
+static lv_chart_series_t *ui_fuelTrimChartBank1Series;
+static lv_chart_series_t *ui_fuelTrimChartBank2Series;
+static lv_obj_t *ui_stft1Label;
+static lv_obj_t *ui_stft2Label;
+static lv_obj_t *ui_ltft1Label;
+static lv_obj_t *ui_ltft2Label;
+
+void ui_setSpeedValue(int32_t value)
+{
+    lv_arc_set_value(ui_speedArc, value);
+    lv_label_set_text(ui_speedLabel, String(value).c_str());
+}
+
+void ui_updateWarningLabel(const char* text, bool prepend = false) {
+    if (prepend) {
+        lv_label_ins_text(ui_warningsLabel, 0, text);
+    } else {
+        lv_label_set_text(ui_warningsLabel, text);
+    }
+}
+
+void ui_updateFuelTrimChart(int bank1Value, int bank2Value)
+{
+    lv_chart_set_next_value(ui_fuelTrimChart, ui_fuelTrimChartBank1Series, bank1Value);
+    lv_chart_set_next_value(ui_fuelTrimChart, ui_fuelTrimChartBank2Series, bank2Value);
+}
+
+inline void updateFuelTrimLabel(lv_obj_t* label, const char* trimName, float trim)
+{
+    lv_label_set_text(label, (String(trimName) + " " + trim + "%").c_str());
+}
+
+void ui_updateStft1Label(float value)
+{
+    updateFuelTrimLabel(ui_stft1Label,"STFT1", value);
+}
+
+void ui_updateStft2Label(float value)
+{
+    updateFuelTrimLabel(ui_stft2Label,"STFT2", value);
+}
+
+void ui_updateLtft1Label(float value)
+{
+    updateFuelTrimLabel(ui_ltft1Label,"LTFT1", value);
+}
+
+void ui_updateLtft2Label(float value)
+{
+    updateFuelTrimLabel(ui_ltft2Label,"LTFT2", value);
+}
+
+
+LilyGo_Class amoled;
+
+void ui_setup()
+{
+    bool rslt =  amoled.beginAMOLED_191();
+
+    if (!rslt) {
+        while (1) {
+            Serial.println("The board model cannot be detected, please raise the Core Debug Level to an error");
+            delay(1000);
+        }
+    }
+
+    beginLvglHelper(amoled);
+
+    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x000000), LV_STATE_DEFAULT);
+
+    ui_warningsLabel = lv_label_create(lv_scr_act());
+    lv_obj_set_width(ui_warningsLabel, 218);
+    lv_obj_set_height(ui_warningsLabel, LV_SIZE_CONTENT);    /// 56
+    lv_obj_set_x(ui_warningsLabel, 14);
+    lv_obj_set_y(ui_warningsLabel, -6);
+    lv_obj_set_align(ui_warningsLabel, LV_ALIGN_BOTTOM_LEFT);
+    lv_label_set_text(ui_warningsLabel, "DTCS: P0123 P0234 P123 P500 P600 P1203");
+    lv_obj_set_style_text_color(ui_warningsLabel, lv_color_hex(0xFAC500), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_warningsLabel, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_align(ui_warningsLabel, LV_TEXT_ALIGN_LEFT, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    ui_fuelTrimChart = lv_chart_create(lv_scr_act());
+    lv_obj_set_width(ui_fuelTrimChart, 257);
+    lv_obj_set_height(ui_fuelTrimChart, 153);
+    lv_obj_set_x(ui_fuelTrimChart, 103);
+    lv_obj_set_y(ui_fuelTrimChart, -32);
+    lv_obj_set_align(ui_fuelTrimChart, LV_ALIGN_CENTER);
+    lv_chart_set_type(ui_fuelTrimChart, LV_CHART_TYPE_LINE);
+    lv_chart_set_point_count(ui_fuelTrimChart, 50);
+    lv_chart_set_range(ui_fuelTrimChart, LV_CHART_AXIS_PRIMARY_Y, -20, 20);
+    lv_chart_set_range(ui_fuelTrimChart, LV_CHART_AXIS_SECONDARY_Y, -20, 20);
+    lv_chart_set_div_line_count(ui_fuelTrimChart, 0, 0);
+    lv_chart_set_axis_tick(ui_fuelTrimChart, LV_CHART_AXIS_PRIMARY_X, 0, 0, 0, 0, false, 50);
+    lv_chart_set_axis_tick(ui_fuelTrimChart, LV_CHART_AXIS_PRIMARY_Y, 10, 5, 5, 2, false, 50);
+    lv_chart_set_axis_tick(ui_fuelTrimChart, LV_CHART_AXIS_SECONDARY_Y, 10, 5, 5, 2, true, 25);
+    lv_chart_series_t * ui_fuelTrimChart_series_1 = lv_chart_add_series(ui_fuelTrimChart, lv_color_hex(0x44FF58),
+                                                                        LV_CHART_AXIS_SECONDARY_Y);
+    static lv_coord_t ui_fuelTrimChart_series_1_array[] = { 15, 20, -5, 5, 0 };
+    lv_chart_set_ext_y_array(ui_fuelTrimChart, ui_fuelTrimChart_series_1, ui_fuelTrimChart_series_1_array);
+    lv_chart_series_t * ui_fuelTrimChart_series_2 = lv_chart_add_series(ui_fuelTrimChart, lv_color_hex(0xFF1313),
+                                                                        LV_CHART_AXIS_SECONDARY_Y);
+    static lv_coord_t ui_fuelTrimChart_series_2_array[] = { 0, -2, 2, 1, 0, 3, -5, 2 };
+    lv_chart_set_ext_y_array(ui_fuelTrimChart, ui_fuelTrimChart_series_2, ui_fuelTrimChart_series_2_array);
+    lv_obj_set_style_radius(ui_fuelTrimChart, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(ui_fuelTrimChart, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_opa(ui_fuelTrimChart, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_color(ui_fuelTrimChart, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_opa(ui_fuelTrimChart, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_width(ui_fuelTrimChart, 1, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_side(ui_fuelTrimChart, LV_BORDER_SIDE_FULL, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    lv_obj_set_style_line_color(ui_fuelTrimChart, lv_color_hex(0xFFFFFF), LV_PART_TICKS | LV_STATE_DEFAULT);
+    lv_obj_set_style_line_opa(ui_fuelTrimChart, 255, LV_PART_TICKS | LV_STATE_DEFAULT);
+    lv_obj_set_style_line_width(ui_fuelTrimChart, 0, LV_PART_TICKS | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_color(ui_fuelTrimChart, lv_color_hex(0xFFFFFF), LV_PART_TICKS | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_fuelTrimChart, 255, LV_PART_TICKS | LV_STATE_DEFAULT);
+
+    ui_speedArc = lv_arc_create(lv_scr_act());
+    lv_obj_set_width(ui_speedArc, 220);
+    lv_obj_set_height(ui_speedArc, 233);
+    lv_obj_set_x(ui_speedArc, 10);
+    lv_obj_set_y(ui_speedArc, 7);
+    lv_arc_set_range(ui_speedArc, 0, 180);
+    lv_arc_set_value(ui_speedArc, 35);
+    lv_arc_set_bg_angles(ui_speedArc, 150, 30);
+    lv_obj_set_style_arc_color(ui_speedArc, lv_color_hex(0x8D8D8D), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_opa(ui_speedArc, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_width(ui_speedArc, 25, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_rounded(ui_speedArc, false, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    lv_obj_set_style_arc_color(ui_speedArc, lv_color_hex(0x3BB8FF), LV_PART_INDICATOR | LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_opa(ui_speedArc, 255, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_width(ui_speedArc, 25, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+    lv_obj_set_style_arc_rounded(ui_speedArc, false, LV_PART_INDICATOR | LV_STATE_DEFAULT);
+
+    lv_obj_set_style_bg_color(ui_speedArc, lv_color_hex(0xFFFFFF), LV_PART_KNOB | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_opa(ui_speedArc, 0, LV_PART_KNOB | LV_STATE_DEFAULT);
+
+    ui_speedLabel = lv_label_create(ui_speedArc);
+    lv_obj_set_width(ui_speedLabel, LV_SIZE_CONTENT);   /// 1
+    lv_obj_set_height(ui_speedLabel, LV_SIZE_CONTENT);    /// 1
+    lv_obj_set_x(ui_speedLabel, 0);
+    lv_obj_set_y(ui_speedLabel, -33);
+    lv_obj_set_align(ui_speedLabel, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_speedLabel, "100");
+    lv_obj_set_style_text_color(ui_speedLabel, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_speedLabel, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_align(ui_speedLabel, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_speedLabel, &lv_font_montserrat_48, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    lv_obj_t* ui_Label8 = lv_label_create(ui_speedArc);
+    lv_obj_set_width(ui_Label8, LV_SIZE_CONTENT);   /// 1
+    lv_obj_set_height(ui_Label8, LV_SIZE_CONTENT);    /// 1
+    lv_obj_set_align(ui_Label8, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_Label8, "km/h");
+    lv_obj_set_style_text_color(ui_Label8, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_Label8, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    ui_ltft1Label = lv_label_create(lv_scr_act());
+    lv_obj_set_width(ui_ltft1Label, 140);
+    lv_obj_set_height(ui_ltft1Label, LV_SIZE_CONTENT);    /// 64
+    lv_obj_set_x(ui_ltft1Label, 43);
+    lv_obj_set_y(ui_ltft1Label, 68);
+    lv_obj_set_align(ui_ltft1Label, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_ltft1Label, "LTFT1 15%");
+    lv_obj_set_style_text_color(ui_ltft1Label, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_ltft1Label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_align(ui_ltft1Label, LV_TEXT_ALIGN_LEFT, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_ltft1Label, &lv_font_montserrat_24, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    ui_stft1Label = lv_label_create(lv_scr_act());
+    lv_obj_set_width(ui_stft1Label, 140);
+    lv_obj_set_height(ui_stft1Label, LV_SIZE_CONTENT);    /// 64
+    lv_obj_set_x(ui_stft1Label, 194);
+    lv_obj_set_y(ui_stft1Label, 69);
+    lv_obj_set_align(ui_stft1Label, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_stft1Label, "STFT1 5%");
+    lv_obj_set_style_text_color(ui_stft1Label, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_stft1Label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_align(ui_stft1Label, LV_TEXT_ALIGN_LEFT, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_stft1Label, &lv_font_montserrat_24, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    ui_stft2Label = lv_label_create(lv_scr_act());
+    lv_obj_set_width(ui_stft2Label, 140);
+    lv_obj_set_height(ui_stft2Label, LV_SIZE_CONTENT);    /// 64
+    lv_obj_set_x(ui_stft2Label, 194);
+    lv_obj_set_y(ui_stft2Label, 104);
+    lv_obj_set_align(ui_stft2Label, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_stft2Label, "STFT2 -25%");
+    lv_obj_set_style_text_color(ui_stft2Label, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_stft2Label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_align(ui_stft2Label, LV_TEXT_ALIGN_LEFT, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_stft2Label, &lv_font_montserrat_24, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    ui_ltft2Label = lv_label_create(lv_scr_act());
+    lv_obj_set_width(ui_ltft2Label, 140);
+    lv_obj_set_height(ui_ltft2Label, LV_SIZE_CONTENT);    /// 64
+    lv_obj_set_x(ui_ltft2Label, 43);
+    lv_obj_set_y(ui_ltft2Label, 104);
+    lv_obj_set_align(ui_ltft2Label, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_ltft2Label, "LTFT2 -5%");
+    lv_obj_set_style_text_color(ui_ltft2Label, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_ltft2Label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_align(ui_ltft2Label, LV_TEXT_ALIGN_LEFT, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_ltft2Label, &lv_font_montserrat_24, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    // My customization
+    ui_fuelTrimChartBank1Series = ui_fuelTrimChart_series_1;
+    ui_fuelTrimChartBank2Series = ui_fuelTrimChart_series_2;
+    lv_chart_set_update_mode(ui_fuelTrimChart, LV_CHART_UPDATE_MODE_SHIFT);
+
+    static lv_coord_t series1_array[50] = {0};
+    static lv_coord_t series2_array[50] = {0};
+    lv_chart_set_ext_y_array(ui_fuelTrimChart, ui_fuelTrimChart_series_1, series1_array);
+    lv_chart_set_ext_y_array(ui_fuelTrimChart, ui_fuelTrimChart_series_2, series2_array);
+}
+
+void ui_loop() {
+    lv_task_handler();
+}
